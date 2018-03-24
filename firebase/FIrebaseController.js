5bbe1b6ac534664cefee5a4474df55d45ea62ba0
Robert <dominicusrobert@yahoo.com>
2018-03-24 14:44:26 +0700

    add firebase

---
+++ firebase/FIrebaseController.js
@@ -0,0 +1,95 @@
+const db = require('./Firebase')
+
+class Firebase {
+
+  static getRestaurantData(restaurantId) {
+    return new Promise(function (resolve, reject) {
+
+      var restaurantRef = db.collection('Restaurants').doc(restaurantId);
+
+      const arrPromise = [
+        restaurantRef.get(),
+        restaurantRef.collection("menu").get(),
+        restaurantRef.collection("tableList").get()
+      ]
+
+      Promise.all(arrPromise)
+        .then(data => {
+          let documentRestaurant = data[0]
+          let snapshotMenu = data[1]
+          let snapshotTableList = data[2]
+
+          let restaurantName = documentRestaurant.data()
+
+          let arrMenu = []
+          snapshotMenu.forEach(doc => {
+            arrMenu.push({
+              id: doc.id,
+              name: doc.data().name,
+              photoUrl: doc.data().photoUrl,
+              description: doc.data().description,
+              price: doc.data().price,
+              category: doc.data().category
+            })
+          })
+
+          let arrTableList = []
+          snapshotTableList.forEach(doc => {
+            arrTableList.push({
+              id: doc.id,
+              name: doc.data().tableName,
+              status: doc.data().tableStatus
+            })
+          })
+
+          const response = {
+            id: restaurantId,
+            name: restaurantName,
+            table_list: arrTableList,
+            menu_list: arrMenu
+          }
+
+          resolve(response)
+        })
+        .catch(err => {
+          reject(err)
+        });
+    })
+  }
+
+  static createOrder(orderData) {
+    return new Promise(function (resolve, reject) {
+      const OrderCollection = db.collection('Orders')
+      OrderCollection.add({
+        idCustomer: orderData.idCustomer,
+        idRestaurant: orderData.idRestaurant,
+        idTable: orderData.idTable,
+      })
+        .then((response) => {
+
+          const arrPromiseMenu = []
+          orderData.menuList.forEach(data => {
+            arrPromiseMenu.push(
+              OrderCollection.doc(response.id).collection('menuList')
+                .add({
+                  menuId: data.menuId,
+                  quantity: data.quantity,
+                  price: data.price
+                })
+            )
+          })
+
+          return Promise.all(arrPromiseMenu)
+        })
+        .then((response) => {
+          resolve('Success')
+        })
+        .catch((error) => {
+          reject(error)
+        });
+    })
+  }
+
+}
+
+module.exports = Firebase
--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var bleno = require('bleno'); // for implementing BLE peripheral
 var Pusher = require('pusher'); // for pushing real-time updates to clients
 var dateFormat = require('dateformat'); // for formatting dates
 
-require('dotenv').config(); // for loading .env file
+var FirebaseDB = require('./firebase/FIrebaseController')
 
-var Pusher = require('pusher');
+require('dotenv').config();
+
 
 var pusher = new Pusher({
   appId: process.env.APP_ID,
@@ -14,27 +15,24 @@ var pusher = new Pusher({
   encrypted: true
 });
 
-pusher.trigger('my-channel', 'my-event', {
- 'message': 'hello world'
-});
 
 var time_format = 'h:MM TT';
 var customers = [
-    {
-        id: 1, 
-        full_name: 'milfa', 
-        time_entered: dateFormat(new Date(1505901033110), time_format)
-    },
-    {
-        id: 2, 
-        full_name: 'red', 
-        time_entered: dateFormat(new Date(1505901733110), time_format)
-    },
-    {
-        id: 3, 
-        full_name: 'silver', 
-        time_entered: dateFormat(new Date(1505908733110), time_format)
-    }
+  {
+    id: 1,
+    full_name: 'milfa',
+    time_entered: dateFormat(new Date(1505901033110), time_format)
+  },
+  {
+    id: 2,
+    full_name: 'red',
+    time_entered: dateFormat(new Date(1505901733110), time_format)
+  },
+  {
+    id: 3,
+    full_name: 'silver',
+    time_entered: dateFormat(new Date(1505908733110), time_format)
+  }
 ];
 
 const BASE_UUID = '-5659-402b-aeb3-d2f7dcd1b999';
@@ -42,47 +40,48 @@ const PERIPHERAL_ID = '0000';
 const PRIMARY_SERVICE_ID = '0100';
 
 var primary_service_uuid = PERIPHERAL_ID + PRIMARY_SERVICE_ID + BASE_UUID;
-var ps_characteristic_uuid = PERIPHERAL_ID + '0300' + BASE_UUID; 
+var ps_characteristic_uuid = PERIPHERAL_ID + '0300' + BASE_UUID;
 
 var settings = {
   service_id: primary_service_uuid,
   characteristic_id: ps_characteristic_uuid
 };
 
-bleno.on('stateChange', function(state){
-  console.log(state)
-  if(state === 'poweredOn'){
-    console.log('msk true')
-    bleno.startAdvertising('FUADI', [settings.service_id], (err) => console.log(err));
-  }else{
-    console.log('msk false');
+bleno.on('stateChange', function (state) {
+  console.log('state bleno', state)
+
+  if (state === 'poweredOn') {
+    bleno.startAdvertising('Fuadi Resto', [settings.service_id], (err) => console.log(err));
+  } else {
     bleno.stopAdvertising();
   }
 });
 
-bleno.on('advertisingStart', function(error){
-  if(error){
+bleno.on('advertisingStart', function (error) {
+  if (error) {
     console.log('something went wrong while trying to start advertisement of services');
-  }else{
+  } else {
     console.log('started..');
     bleno.setServices([
       new bleno.PrimaryService({ // create a service
-        uuid : settings.service_id,
-        characteristics : [
+        uuid: settings.service_id,
+        characteristics: [
           new bleno.Characteristic({ // add a characteristic to the service
-            value : null,
-            uuid : settings.characteristic_id,
-            properties : ['write'],
-            onWriteRequest : function(data, offset, withoutResponse, callback){
+            value: null,
+            uuid: settings.characteristic_id,
+            properties: ['write'],
+            onWriteRequest: function (data, offset, withoutResponse, callback) {
               console.log(data, 'buffer kkenya')
-              var customer = JSON.parse(data.toString());
-              customer.time_entered = dateFormat(new Date(), time_format); 
-              customers.push(customer); 
-              console.log(customers);
+              var customer = JSON.parse(data.toString())
+              customer.time_entered = dateFormat(new Date(), time_format)
+              customers.push(customer)
+              console.log(customers)
 
-              pusher.trigger('attendance-channel', 'attendance-event', customer); // send the new customer's data to all clients
+              // send the new customer's data to all clients
+              pusher.trigger('attendance-channel', 'attendance-event', customer)
 
-              callback(this.RESULT_SUCCESS); // tell the client that the request has succeeded
+              // tell the client that the request has succeeded
+              callback(this.RESULT_SUCCESS)
             }
           })
         ]
@@ -91,11 +90,22 @@ bleno.on('advertisingStart', function(error){
   }
 });
 
-bleno.on('accept', function(clientAddress){
+bleno.on('accept', function (clientAddress) {
   console.log('client address: ', clientAddress);
-  var data = {
-    is_customers: true,
-    customers: customers
-  };
-  pusher.trigger('attendance-channel', 'attendance-event', data);
+
+  FirebaseDB.getRestaurantData('UwtUW7ny5SPbbCWE8rWv')
+  .then((data)=>{
+    console.log('success Firebase', data)
+    pusher.trigger('attendance-channel', 'attendance-event', data);
+    
+  })
+  .catch((err)=>{
+    console.error('eror Firebase', err)
+  })
+
+  // var data = {
+  //   is_customers: true,
+  //   customers: customers
+  // };
+  // pusher.trigger('attendance-channel', 'attendance-event', data);
 });
